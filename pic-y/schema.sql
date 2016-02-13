0df53a925652682e9cdc93b9c3352dfd4c93125c
Vivian Fang <vivian98.fang@gmail.com>
2016-02-13 15:19:29 -0800

    New file upload schemea

--- pic-y/schema.sql
+++ pic-y/schema.sql
@@ -1,12 +1,17 @@
 drop table if exists users;
 create table users (
-    # MICHAEL FILL THIS OUT
+    user text
 );
 
 drop table if exists food;
 create table food (
 	id integer primary key autoincrement,
+	user text not null,
 	title text not null,
 	description text,
-	theme text not null
-)
+	location text,
+	theme text not null,
+	url text not null
+);
+
+insert into food values(1, 'bob', 'title', 'desc', 'loc', 'potato', '1.jpg');
