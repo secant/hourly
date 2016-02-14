521a46bdecf69fe345de5b0bd01aee4e12ad9c21
Vivian Fang <vivian98.fang@gmail.com>
2016-02-13 17:14:25 -0800

    resolving schema conflict

--- pic-y/schema.sql
+++ pic-y/schema.sql
@@ -11,13 +11,13 @@ create table users (
 
 drop table if exists food;
 create table food (
-	id integer primary key autoincrement,
-	user text not null,
-	title text not null,
-	description text,
-	location text,
-	theme text not null,
-	url text not null
+    id integer primary key autoincrement,
+    user text not null,
+    title text not null,
+    description text,
+    location text,
+    theme text not null,
+    url text not null
 );
 
 insert into food values(1, 'bob', 'title', 'desc', 'loc', 'potato', '1.jpg');
